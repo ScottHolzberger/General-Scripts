0d97bf9f644f336fff08260cc059956972a31d1a
Scott Holzberger <scott@zahezone.com.au>
2025-07-11 17:33:47 +1000

    New Scripts

--- OSDCloudTemplate.ps1
+++ OSDCloudTemplate.ps1
@@ -6,11 +6,11 @@ Import-Module OSD -Force
  
 New-OSDCloudTemplate
  
-New-OSDCloudWorkspace -WorkspacePath C:\OSDCloud
+New-OSDCloudWorkspace -WorkspacePath C:\OSDCloud2
  
 New-OSDCloudUSB
  
-Edit-OSDCloudwinPE -workspacepath C:\OSDCloud -CloudDriver * -WebPSScript https://raw.githubusercontent.com/ScottHolzberger/General-Scripts/6a6f04abd01dbed901dd1ca1d669f4f8482809ea/osdcloud_config.ps1 -Verbose
+Edit-OSDCloudwinPE -workspacepath C:\OSDCloud2 -CloudDriver * -WebPSScript https://raw.githubusercontent.com/ScottHolzberger/General-Scripts/6a6f04abd01dbed901dd1ca1d669f4f8482809ea/osdcloud_config.ps1 -Verbose
  
 New-OSDCloudISO
  
