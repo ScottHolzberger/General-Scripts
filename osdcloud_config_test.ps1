179eb0a62cbf1192764e265e0b5bd771d0ce0a9f
Scott Holzberger <scott@zahezone.com.au>
2025-12-15 17:55:41 +1000

    OSDConfig Testing 25H2

---
+++ osdcloud_config_test.ps1
@@ -0,0 +1,12 @@
+Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
+Start-Sleep -Seconds 5
+
+Start-OSDCloud -OSName 'Windows 11 25H2 x64' -OSActivation Retail -OSLanguage en-us -OSEdition Pro
+
+#Restart from WinPE
+
+Write-Host -ForegroundColor Green “Restarting in 20 seconds!”
+
+Start-Sleep -Seconds 20
+
+wpeutil reboot
