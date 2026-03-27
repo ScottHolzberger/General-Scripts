d54d1b47fada5af5fdaf92ba61da25da28cf9d60
Scott Holzberger <scott@zahezone.com.au>
2026-03-27 11:47:48 +1000

    Update_NewPC

---
+++ Force Windows Updates.ps1
@@ -0,0 +1,23 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$providerName = "NuGet"
+if ($null -eq (Get-PackageProvider -Name $providerName -ListAvailable -ErrorAction Ignore)) {
+    Write-Host "Installing $providerName package provider..."
+    Install-PackageProvider -Name $providerName -Force -ForceBootstrap -Verbose
+} else {
+    Write-Host "$providerName package provider is already installed."
+}
+
+$moduleName = "PSWindowsUpdate"
+if (Get-Module -ListAvailable -Name $moduleName) {
+    Write-Host "The $moduleName module is installed and available."
+} else {
+    Write-Host "The $moduleName module is not installed. Installing now..."
+    Install-Module -Name PSWindowsUpdate -Force -Confirm:$False
+    
+}
+
+#Install-Module -Name PSWindowsUpdate -Force -Confirm:$False
+Import-Module PSWindowsUpdate
+#Get-WindowsUpdate -AcceptAll -Install -AutoReboot
+Get-WindowsUpdate -MicrosoftUpdate -AcceptAll -Install -AutoReboot -Verbose
---
+++ OOO Messages.ps1
@@ -0,0 +1,91 @@
+﻿Connect-ExchangeOnline
+
+Set-MailboxAutoReplyConfiguration -Identity "edwin@curiofinancial.au" -AutoReplyState Scheduled -StartTime "2025-12-19T09:00:00" -EndTime "2026-01-12T09:00:00" -ExternalMessage "Thank you for your email.<br>
+<br> 
+I am currently out of the office and will return on Monday 12th January 2026. Emails will be monitored during this time; however, responses may be delayed.<br>
+ <br>
+If your matter is urgent, please contact admin@curiofinancial.au <br>
+ <br>
+On behalf of the Curio team, we wish you a Merry Christmas and a safe, happy holiday period.<br>
+<br>"
+
+Set-MailboxAutoReplyConfiguration -Identity "eloise@curiofinancial.au" -AutoReplyState Scheduled -StartTime "2025-12-19T09:00:00" -EndTime "2026-01-12T09:00:00" -ExternalMessage "Thank you for your email.<br>
+<br> 
+I am currently out of the office and will return on Monday 12th January 2026. Emails will be monitored during this time; however, responses may be delayed.<br>
+ <br>
+If your matter is urgent, please contact admin@curiofinancial.au <br>
+ <br>
+On behalf of the Curio team, we wish you a Merry Christmas and a safe, happy holiday period.<br>
+<br>"
+
+Set-MailboxAutoReplyConfiguration -Identity "john@curiofinancial.au" -AutoReplyState Scheduled -StartTime "2025-12-19T09:00:00" -EndTime "2026-01-12T09:00:00" -ExternalMessage "Thank you for your email.<br>
+<br> 
+I am currently out of the office and will return on Monday 12th January 2026. Emails will be monitored during this time; however, responses may be delayed.<br>
+ <br>
+If your matter is urgent, please contact admin@curiofinancial.au <br>
+ <br>
+On behalf of the Curio team, we wish you a Merry Christmas and a safe, happy holiday period.<br>
+<br>"
+
+Set-MailboxAutoReplyConfiguration -Identity "julia@curiofinancial.au" -AutoReplyState Scheduled -StartTime "2025-12-19T09:00:00" -EndTime "2026-01-12T09:00:00" -ExternalMessage "Thank you for your email.<br>
+<br> 
+I am currently out of the office and will return on Monday 12th January 2026. Emails will be monitored during this time; however, responses may be delayed.<br>
+ <br>
+If your matter is urgent, please contact admin@curiofinancial.au <br>
+ <br>
+On behalf of the Curio team, we wish you a Merry Christmas and a safe, happy holiday period.<br>
+<br>"
+
+Set-MailboxAutoReplyConfiguration -Identity "katie@curiofinancial.au" -AutoReplyState Scheduled -StartTime "2025-12-19T09:00:00" -EndTime "2026-01-12T09:00:00" -ExternalMessage "Thank you for your email.<br>
+<br> 
+I am currently out of the office and will return on Monday 12th January 2026. Emails will be monitored during this time; however, responses may be delayed.<br>
+ <br>
+If your matter is urgent, please contact admin@curiofinancial.au <br>
+ <br>
+On behalf of the Curio team, we wish you a Merry Christmas and a safe, happy holiday period.<br>
+<br>"
+
+Set-MailboxAutoReplyConfiguration -Identity "tanya@curiofinancial.au" -AutoReplyState Scheduled -StartTime "2025-12-19T09:00:00" -EndTime "2026-01-12T09:00:00" -ExternalMessage "Thank you for your email.<br>
+<br> 
+I am currently out of the office and will return on Monday 12th January 2026. Emails will be monitored during this time; however, responses may be delayed.<br>
+ <br>
+If your matter is urgent, please contact admin@curiofinancial.au <br>
+ <br>
+On behalf of the Curio team, we wish you a Merry Christmas and a safe, happy holiday period.<br>
+<br>"
+
+Set-MailboxAutoReplyConfiguration -Identity "tiana@curiofinancial.au" -AutoReplyState Scheduled -StartTime "2025-12-19T09:00:00" -EndTime "2026-01-12T09:00:00" -ExternalMessage "Thank you for your email.<br>
+<br> 
+I am currently out of the office and will return on Monday 12th January 2026. Emails will be monitored during this time; however, responses may be delayed.<br>
+ <br>
+If your matter is urgent, please contact admin@curiofinancial.au <br>
+ <br>
+On behalf of the Curio team, we wish you a Merry Christmas and a safe, happy holiday period.<br>
+<br>"
+
+Set-MailboxAutoReplyConfiguration -Identity "tom@curiofinancial.au" -AutoReplyState Scheduled -StartTime "2025-12-19T09:00:00" -EndTime "2026-01-12T09:00:00" -ExternalMessage "Thank you for your email.<br>
+<br> 
+I am currently out of the office and will return on Monday 12th January 2026. Emails will be monitored during this time; however, responses may be delayed.<br>
+ <br>
+If your matter is urgent, please contact admin@curiofinancial.au <br>
+ <br>
+On behalf of the Curio team, we wish you a Merry Christmas and a safe, happy holiday period.<br>
+<br>"
+
+Set-MailboxAutoReplyConfiguration -Identity "ziggy@curiofinancial.au" -AutoReplyState Scheduled -StartTime "2025-12-19T09:00:00" -EndTime "2026-01-12T09:00:00" -ExternalMessage "Thank you for your email.<br>
+<br> 
+I am currently out of the office and will return on Monday 12th January 2026. Emails will be monitored during this time; however, responses may be delayed.<br>
+ <br>
+If your matter is urgent, please contact admin@curiofinancial.au <br>
+ <br>
+On behalf of the Curio team, we wish you a Merry Christmas and a safe, happy holiday period.<br>
+<br>"
+
+Set-MailboxAutoReplyConfiguration -Identity "admin@curiofinancial.au" -AutoReplyState Scheduled -StartTime "2025-12-19T09:00:00" -EndTime "2026-01-12T09:00:00" -ExternalMessage "Thank you for your email.<br>
+<br> 
+I am currently out of the office and will return on Monday 12th January 2026. Emails will be monitored during this time; however, responses may be delayed.<br>
+ <br>
+If your matter is urgent, please contact admin@curiofinancial.au <br>
+ <br>
+On behalf of the Curio team, we wish you a Merry Christmas and a safe, happy holiday period.<br>
+<br>"
